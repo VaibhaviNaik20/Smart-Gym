e29988110738f73a7da2ce4e5f30dc12742f8e0b
vaibhaviparimalnaik <vaibhaviparimalnaik@CSUS-SL-28560.saclink.csus.edu>
2025-11-06 17:10:01 -0800

    Added Hello.java with master info on line 8

--- src/lab02/Hello.java
+++ src/lab02/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 		System.out.println("Welcome to Git!");
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
-		System.out.println("303834921 - This line was added by Vaibhavi name Naik.");
+		System.out.println("303834921 - This line was added by Vaibhavi Naik, I'm the git Master for this project.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
